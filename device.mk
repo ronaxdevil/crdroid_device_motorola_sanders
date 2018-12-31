dbb020ce5e72b476ca8f7b7053c5a36432303590
ronaxdevil <pratabidya.007@gmail.com>
2018-12-31 12:40:24 +0000

    sanders: Add QCOM's WFD implementation
    * Sadly, out-of-the-box Miracast support was killed in P, so
      we need this to keep Wi-Fi display feature working.
    
    Author:     Artem Borisov <dedsa2002@gmail.com>
    AuthorDate: 2018-12-24 15:25:51 +0400
    (Author for sdm845-common)

--- device.mk
+++ device.mk
@@ -442,3 +442,12 @@ PRODUCT_COPY_FILES += \
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
+
+# Wi-Fi Display
+PRODUCT_PACKAGES += \
+    libnl \
+    vendor.display.config@1.0
+
+PRODUCT_BOOT_JARS += \
+    WfdCommon
+
