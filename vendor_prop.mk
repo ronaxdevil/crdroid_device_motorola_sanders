09dfefb27ae2fb48afe51d3718744afb908ca9f3
ronaxdevil <pratabidya.007@gmail.com>
2019-03-16 08:09:06 +0000

    vendor_prop: Tweaks For Better Ram Management

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -353,3 +353,28 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.debug.wfd.enable=1 \
     vendor.video.disable.ubwc=1
+
+# FOSS CONFIGS
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.qcom.dpps.sensortype=3 \
+    ro.qualcomm.display.paneltype=1 \
+    ro.qualcomm.foss=1 \
+    config.foss.xml=1 \
+    config.foss.path=/vendor/etc/FOSSConfig.xml
+
+# Create Swap disk, if below sys-prop enabled & also if device has lower (< 1 GB) RAM
+PRODUCT_PROPERTY_OVERRIDES += \
+ro.config.swap=true
+
+# Properties
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.config.fha_enable=true \
+    ro.sys.fw.bg_apps_limit=32 \
+    ro.config.dha_cached_max=16 \
+    ro.config.dha_empty_max=42 \
+    ro.config.dha_empty_init=32 \
+    ro.config.dha_lmk_scale=0.545 \
+    ro.config.dha_th_rate=2.3 \
+    ro.config.sdha_apps_bg_max=64 \
+    ro.config.sdha_apps_bg_min=8
+
